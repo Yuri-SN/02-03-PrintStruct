dfdd21b83b7e40b446aed2d5dab603755175e94f
Yuri Solyanik <jura.s.n@gmail.com>
2024-09-19 14:04:24 +0500

    Работа над ошибками: теперь передаю структуры в функцию по константной ссылке

--- main.cpp
+++ main.cpp
@@ -8,34 +8,22 @@ struct Address {
     uint postalCode;
 };
 
-void printAddress(Address address) {
-    std::cout << "Город: " << address.city << std::endl;
-    std::cout << "Улица: " << address.street << std::endl;
-    std::cout << "Номер дома: " << address.house << std::endl;
-    std::cout << "Номер квартиры: " << address.apartment << std::endl;
-    std::cout << "Индекс: " << address.postalCode << std::endl;
+void printAddress(const Address *address) {
+    std::cout << "Город: " << address->city << std::endl;
+    std::cout << "Улица: " << address->street << std::endl;
+    std::cout << "Номер дома: " << address->house << std::endl;
+    std::cout << "Номер квартиры: " << address->apartment << std::endl;
+    std::cout << "Индекс: " << address->postalCode << std::endl;
+
     std::cout << std::endl;
 }
 
-int main()
-{
-    Address address1 {
-        "Москва",
-        "Арбат",
-        "12",
-        "8",
-        123456
-    };
-    printAddress(address1);
+int main() {
+    Address address1{"Москва", "Арбат", "12", "8", 123456};
+    printAddress(&address1);
 
-    Address address2 {
-        "Ижевск",
-        "Пушкина",
-        "59",
-        "143",
-        953769
-    };
-    printAddress(address2);
+    Address address2{"Ижевск", "Пушкина", "59", "143", 953769};
+    printAddress(&address2);
 
     return 0;
 }
